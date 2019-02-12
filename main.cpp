7de47c78b6ee4843f87bdaea05467581a9b996e9
Dmitrii Bondarenko <bondarenkoda@gmail.com>
2019-02-12 08:34:58 +0500

    added best/worst case switch

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@
 #include "oncache.h"
 #include "stlcache.h"
 
-#define TESTMAXSIZE 100000
-#define LOGPOINT 10000
+#define TEST_MAXSIZE 1000000
+#define START_LOGPOINT 1000
 #define TEST_CASE_FILE
 
 template <typename T>
@@ -203,41 +203,38 @@ bool  mkdir_p(const char  *filePath)  {
 bool  saveTFileS(const char  *filePath,  const char  *data,  unsigned long  len)  {
   bool re  =  false;
   if (mkdir_p(filePath))  {
-    std::ofstream outfile  (filePath);
-        outfile.write (data, len);
-        outfile.close();
-        re = true;
-    }
-    return re;
+    std::ofstream outfile  (filePath);        
+    outfile.write (data,  len);
+    outfile.close();
+    re  =  true;
+  }
+  return re;
 }
 
-std::string loadFileS(const char * filePath) {
-    std::string str;
-
-        std::ifstream t(filePath);
-        t.seekg(0, std::ios::end);
-        str.reserve(t.tellg());
-        t.seekg(0, std::ios::beg);
-
-        str.assign((std::istreambuf_iterator<char>(t)),
-                   std::istreambuf_iterator<char>());
-
-    return str;
+std::string  loadFileS(const char  *filePath)  {
+  std::string str;
+  std::ifstream t(filePath);
+  t.seekg(0,  std::ios::end);
+  str.reserve(t.tellg());
+  t.seekg(0, std::ios::beg);
+  str.assign((std::istreambuf_iterator<char>(t)),
+                   std::istreambuf_iterator<char>());    
+  return str;
 }
 
-long long stoll(const char * &p, const char * end) {
-    unsigned long long re = 0ULL; //18 446 744 073 709 551 615 = 20
-    while (*p!=',' && *p && p<end && re<LLONG_MAX){
-        re = 10 * re + *p - '0';
-        ++p;
-    }
+long long  stoll(const char * &p,  const char  *end)  {
+  unsigned long long re  =  0ULL; //18 446 744 073 709 551 615 = 20
+  while (*p != ','  &&  *p  &&  p < end  &&  re < LLONG_MAX)  {
+    re  =  10 * re  +  *p  -  '0';
     ++p;
-    return re;
+  }
+  ++p;
+  return re;
 }
 
-std::string getExePathS(){
-    std::string _exePath;
-    char buf[PATH_MAX];
+std::string  getExePathS()  {
+  std::string  _exePath;
+  char  buf[PATH_MAX];
 #if defined(Windows)
     DWORD len = GetModuleFileNameA(GetModuleHandleA(0x0), buf, MAX_PATH);
 
@@ -249,143 +246,137 @@ std::string getExePathS(){
             }
         }
     }
-#else
-    ssize_t len = 0;
-    len = readlink("/proc/self/exe", buf, PATH_MAX);
-    if (len>0) {
-        for(--len; len>0; --len) {
-            if ('/'==buf[len]){
-                _exePath = std::string(buf, len);
-                break;
-            }
-        }
+#else    
+  ssize_t  len  =  0;
+  len  =  readlink("/proc/self/exe",  buf,  PATH_MAX);
+  if (len  >  0)  {
+    for (--len;  len  >  0;  --len)  {
+      if ('/'  ==  buf[len])  {
+        _exePath  =  std::string(buf, len);
+        break;
+      }
     }
-#endif
-    return _exePath;
+  }
+#endif    
+  return _exePath;
 }
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    srand(std::time(nullptr));
-    //std::map<int, Elem *> mData;
-    std::map<TKey, Elem *> mData;
-    unsigned long nextLogPoint = LOGPOINT;
+int main(int  argc,  char  *argv[])  {
+  /* params: */
+  //  if you need same test with same data from previos run:
+  bool  fromFile  =  false;
+  //  change hash function for best/worst case:
+  TKey::do_worst_case  =  true;
 
-    const std::string &exePath = getExePathS();
-    std::string csvPath(exePath);
 
-    auto start = std::chrono::system_clock::now();
-    long long s = std::chrono::duration_cast<std::chrono::seconds>(
-                   start.time_since_epoch()).count();
+  QCoreApplication a(argc, argv);
+  srand(std::time(nullptr));
+  std::map<TKey, Elem *>  mData;
+  unsigned long  nextLogPoint  =  START_LOGPOINT;
+  const std::string  &exePath  =  getExePathS();
+  std::string csvPath(exePath);
+  auto  start  =  std::chrono::system_clock::now();
+  long long  s  =  std::chrono::duration_cast<std::chrono::seconds>(
+    start.time_since_epoch()).count();
 
-    csvPath.append("/cache.compare.")
+  csvPath.append("/cache.compare.")
             .append(to_string(s))
-            .append(".csv");
-    std::cout<<"Test results will be written to:\n"
-            <<csvPath<<'\n';
-    std::ofstream csvfile (csvPath);
-    csvPath.clear();
-    csvPath.append("Test name");
+            .append(".csv");  
+  std::cout<<"Test results will be written to:\n"
+            <<  csvPath  <<  '\n';
+  std::ofstream  csvfile (csvPath);
+  csvPath.clear();
+  csvPath.append("Test name");
 
-    bool fromFile = false;
+
 #ifdef TEST_CASE_FILE
-    if (fromFile) {
-        std::string testCaseFile(exePath);
-        testCaseFile.append("/testCase.txt");
-        const std::string &testCase = loadFileS(testCaseFile.c_str());
-        const char * begin = testCase.c_str();
-        const char * end   = begin + testCase.length();
-        unsigned long i=0;
-        while (begin < end) {
-            int r = stoll(begin, end);            
-            Elem * e= new Elem(r,r,r,to_string(r));
-            mData.insert(std::make_pair(e->key, e));
-            ++i;
-            if (i>=nextLogPoint) {
-                nextLogPoint+=LOGPOINT;
-                csvPath.append(",")
-                        .append(to_string(i));
-            }
+    if (fromFile)  {
+      std::string  testCaseFile(exePath);
+      testCaseFile.append("/testCase.txt");
+      const std::string  &testCase  =  loadFileS(testCaseFile.c_str());
+      const char  *begin  =  testCase.c_str();
+      const char  *end  =  begin  +  testCase.length();
+      unsigned long  i  =  0;
+      while (begin  <  end)  {
+        int  r  =  stoll(begin,  end);
+        Elem  *e  =  new Elem(r,  r,  r,  to_string(r));
+        mData.insert(std::make_pair(e->key,  e));
+        ++i;
+        if (i  >=  nextLogPoint)  {
+          nextLogPoint  *=  10;
+          csvPath.append(",")
+                        .append(to_string(i));            
         }
-
-    } else {
-        std::string testCase;
-#endif
-        for (unsigned long i=0; i<TESTMAXSIZE; ++i){
-            int r = rand();
-#ifdef TEST_CASE_FILE
-            testCase.append(to_string(r)).push_back(',');
-#endif
-            Elem * e= new Elem(r,r,r,to_string(r));
-            mData.insert(std::make_pair(e->key, e));
-
-            if (i>=nextLogPoint) {
-                nextLogPoint+=LOGPOINT;
-                csvPath.append(",")
-                        .append(to_string(i));
-            }
+      }
+    }  else  {
+      std::string testCase;
+#endif        
+      for (unsigned long  i=0;  i  <=  TEST_MAXSIZE;  ++i)  {
+        int  r  =  rand();
+#ifdef TEST_CASE_FILE            
+        testCase.append(to_string(r)).push_back(',');
+#endif            
+        Elem  *e  =  new Elem(r,  r,  r,  to_string(r));
+        mData.insert(std::make_pair(e->key,  e));
+        if  (i  >=  nextLogPoint)  {
+          nextLogPoint  *=  10;
+          csvPath.append(",")
+              .append(to_string(i));
         }
-#ifdef TEST_CASE_FILE
-        std::string testCaseFile(exePath);
-        testCaseFile.append("/testCase.txt");
-        saveTFileS(testCaseFile.c_str()
-               ,testCase.c_str(), testCase.length());
+      }
+#ifdef TEST_CASE_FILE        
+      std::string testCaseFile(exePath);
+      testCaseFile.append("/testCase.txt");
+      saveTFileS(testCaseFile.c_str(),
+        testCase.c_str(),  testCase.length());
     }
 #endif
 
-    csvPath.append(",")
-            .append(to_string(mData.size())).push_back('\n');
+//    csvPath.append(",")
+//            .append(to_string(mData.size())).push_back('\n');
+    csvPath.push_back('\n');
     csvfile.write (csvPath.c_str(), csvPath.length());
 
-
-    std::cout <<"mData.size()="<<mData.size()<<'\n';
+    std::cout  <<  "mData.size()="  <<  mData.size()  <<'\n';
 
 //    for (auto it : mData) {
 //        std::cout <<it.first<<"\t:\t"<<it.second.data<<'\n';
 //    }
 
-    //НАдо сделать так чтобы тест целиком прогонялся на контейнерах разного объёма
-     //       т.е. тест на 1000, 1000*10, *10 = Тогда O(n) получится
+    std::string  strInsert;
+    std::string  strFind;
+    typedef  void(*TFunc)(const std::map<TKey, Elem *>  &mData,
+      std::string  &strInsert,  std::string  &strFind,
+      unsigned int  testSize,  unsigned int  capacity);
+    std::map<std::string, TFunc>  mFun;
 
-    std::string strInsert;
-    std::string strFind;
-
-    typedef  void(*TFunc)(const std::map<TKey, Elem *> & mData,
-                          std::string & strInsert,
-                          std::string & strFind,
-                          unsigned int testSize, unsigned int capacity);
-    std::map<std::string, TFunc> mFun;
-    //mFun.insert(std::make_pair(std::string("SpeChache"),testSpeChache));
-    //mFun.insert(std::make_pair(std::string("OChache"),testOChache));
-
-
-    //mFun.insert(std::make_pair(std::string("QChache"),  testQChache));
-    //mFun.insert(std::make_pair(std::string("STLChache"),  testSTLChache));
-    mFun.insert(std::make_pair(std::string("OnChache"),testOnChache));
-    for (auto p: mFun){
-        //*p(mData, strInsert, strFind, testSize, capacity);
-        strInsert.clear();
-        strInsert.append(p.first)
-                .append(".insert (micros)");
-        strFind.clear();
-        strFind.append(p.first)
-                .append(".find (micros)");
-        unsigned int testSize = 0;//LOGPOINT;
-        unsigned int capacity = 0;//testSize/10;
-        p.second(mData, strInsert, strFind, 10000, 1000);
-        p.second(mData, strInsert, strFind, 100000, 10000);
+    //Used alhorithms:
+    mFun.insert(std::make_pair(std::string("QChache"),  testQChache));
+    mFun.insert(std::make_pair(std::string("STLChache"),  testSTLChache));
+    mFun.insert(std::make_pair(std::string("OnChache"),  testOnChache));
+    for (auto&&  p:  mFun)  {
+        //*p(mData, strInsert, strFind, testSize, capacity);        
+      strInsert.clear();
+      strInsert.append(p.first)
+                .append(".insert (micros)");        
+      strFind.clear();
+      strFind.append(p.first)
+                .append(".find (micros)");        
+      unsigned int  testSize  =  START_LOGPOINT;
+      unsigned int  capacity  =  testSize / 10;
         //p.second(mData, strInsert, strFind, 10000, 1000);
-//        while(testSize<TESTMAXSIZE){
-//            testSize+=LOGPOINT;
-//            capacity=testSize;// /10;
-//            p.second(mData, strInsert, strFind, testSize, capacity);
-//        }
-        strInsert.push_back('\n');
-        strFind.push_back('\n');
-        csvfile.write (strInsert.c_str(), strInsert.length());
-        csvfile.write (strFind.c_str(), strFind.length());
-    }
+        //p.second(mData, strInsert, strFind, 100000, 10000);
+        //p.second(mData, strInsert, strFind, 10000, 1000);        
+      while (testSize  <=  TEST_MAXSIZE)  {
+        p.second(mData,  strInsert,  strFind,  testSize,  capacity);
+        capacity  =  testSize;
+        testSize  *= 10;
+      }
+      strInsert.push_back('\n');
+      strFind.push_back('\n');
+      csvfile.write (strInsert.c_str(), strInsert.length());
+      csvfile.write (strFind.c_str(), strFind.length());
+    }  // main
 
 
 
